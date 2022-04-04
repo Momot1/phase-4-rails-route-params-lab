3067b6ae546ece7e3677a8a8817c34192f1d8e41
Mark Momot <markmomot03@gmail.com>
2022-04-04 16:08:21 -0400

    commit tree 9469b7e2af5f95df610d2bd63c554ae4d94ac7a6

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,20 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    if params[:name]
+      students = Student.where("LOWER(last_name) = ?", params[:name].downcase)
+      if students ==  nil
+        students = Student.where("LOWER(first_name) = ?", params[:name].downcase)
+      end
+    else
+      students = Student.all
+    end 
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
